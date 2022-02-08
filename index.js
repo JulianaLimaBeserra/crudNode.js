79258e7df13cc4deb6342bdb9f7e7b9356c98d1a
Juliana Lima <julianalimab88@gmail.com>
2022-02-08 16:31:34 -0300

    Adicionando front end com adesao da framework bootstrap 5.1

--- index.js
+++ index.js
@@ -1,56 +1,53 @@
-const express = require('express');
+const express = require("express");
 // const req = require('express/lib/request');
 
 const server = express();
 
 server.use(express.json());
 
-const cursos = ['FullStack Master', 'Desenvolvimento de Games', 'Viver de Youtube'];
+const cursos = [
+  "FullStack Master",
+  "Desenvolvimento de Games",
+  "Viver de Youtube",
+];
 
 // Retorna um curso
-server.get('/cursos/:index', (req, res) => {
-    const { index } = req.params;
-    
-    console.log(index);
-    return res.json(cursos[index]);
+server.get("/cursos/:index", (req, res) => {
+  const { index } = req.params;
 
+  console.log(index);
+  return res.json(cursos[index]);
 });
 
 // Retorna todos os cursos
-server.get('/cursos', (req, res) => {
-    return res.json(cursos);
-
+server.get("/cursos", (req, res) => {
+  return res.json(cursos);
 });
 
-
 // Criar novo curso
-server.post('/cursos', (req, res) => {
-    const { name } = req.body;
-    cursos.push(name);
+server.post("/cursos", (req, res) => {
+  const { name } = req.body;
+  cursos.push(name);
 
-    return res.json(cursos);
+  return res.json(cursos);
 });
 
 //Atualizzar um curso
-server.put('/cursos/:index', (req, res) => {
-    const  { index } = req.params;
-    const { name } = req.body;
+server.put("/cursos/:index", (req, res) => {
+  const { index } = req.params;
+  const { name } = req.body;
 
-    cursos [index] = name;
+  cursos[index] = name;
 
-    return res.json (cursos);
+  return res.json(cursos);
 });
 
 // Deletar um curso
-server.delete('/cursos/:index', (req, res) => {
-    const { index } = req.params;
+server.delete("/cursos/:index", (req, res) => {
+  const { index } = req.params;
 
-    cursos.splice(index, 1);
-    return res.json({message: "O curso foi deletado" })
-
+  cursos.splice(index, 1);
+  return res.json({ message: "O curso foi deletado" });
 });
-
-
-
 
 server.listen(3000);
