f55c5d3dd85dfa92cccb4ad085317cfbef77d048
Juliana Lima <julianalimab88@gmail.com>
2022-02-21 19:30:43 -0300

    Mais algumas alteracoes no layout home

--- client/style.js
+++ client/style.js
@@ -1,4 +1,2 @@
 const currentYear = new  Date();
-document.getElementById("year").innerHTML = currentYear.toDateString();
-//const currentYear = new Date(getFullYear);
-//const year = currentYear.getFullYear();
+document.getElementById("year").innerHTML = currentYear.toLocaleString();
